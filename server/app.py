c4fa77f422afaa79b0ede1bab2832750942915ff
James <james.maskelony@gmail.com>
2018-12-22 18:40:13 -0500

    dockerized, need to fix client seg fault

--- server/app.py
+++ server/app.py
@@ -5,11 +5,13 @@ them to files
 disclaimer: this doesn't work yet
 @author: degenerat3
 """
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/api/rule_send')
+@app.route('/api/rule_send', methods=['GET','POST'])
 def get_rules():
+	content = request.json
+	print content
 	return "we got some rules"
 
 if __name__ == '__main__':
