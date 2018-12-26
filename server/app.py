49a59e7b316b2248ab66d1a7a3c81b1af2b1a3c7
James <james.maskelony@gmail.com>
2018-12-26 16:26:36 -0500

    basic functionality done

--- server/app.py
+++ server/app.py
@@ -6,13 +6,49 @@ disclaimer: this doesn't work yet
 @author: degenerat3
 """
 from flask import Flask, request
+import os
+import datetime
+
 app = Flask(__name__)
 
 @app.route('/api/rule_send', methods=['GET','POST'])
 def get_rules():
 	content = request.json
 	print content
-	return "we got some rules"
+	hostname = content['hostname']
+	hostname = hostname.lower()
+	rules = content['rules']
+	if not os.path.exists("/tmp/flask_files"):
+		os.makedirs("/tmp/flask_files")	
+	dir_str = "/tmp/flask_files/" + hostname + ".txt"
+	with open(dir_str, "w+") as f:
+		header_str = "Hostname: " + hostname
+		t = datetime.datetime.now()
+		t_str = "Updated at: " + str(t)
+		f.write(header_str)
+		f.write("\n")
+		f.write(t_str)
+		f.write("\n\n\n")
+		f.write(rules)
+	return str(content)
+
+
+@app.route('/api/hosts/<host>')
+def view_rules(host):
+	try:
+		dir_str = "/tmp/flask_files/" + host + ".txt"
+		with open(dir_str, "r") as f:
+			data = f.readlines()
+	except:
+		err = "No data on host: " + host
+		return err
+
+	fin = "<p>"
+	for line in data:
+		fin += line + "<br />"
+	fin += "</p>"
+	return fin
+
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
