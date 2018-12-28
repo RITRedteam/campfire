fc7777698079a35d034f7d0bd7ec5cbd34459ee8
James <james.maskelony@gmail.com>
2018-12-27 19:37:02 -0500

    tracker implemented

---
+++ tracker/tracker.py
@@ -0,0 +1,82 @@
+"""
+This script will hit the Campfire API and function as a CLI
+to view tracked hosts' firewall configurations
+@author: degenerat3
+"""
+
+import requests
+import re
+
+server = "127.0.0.1:5000"		#IP of flask server
+
+def list_hosts(srv):
+	endpoint = "http://" + srv + "/hosts/"
+	r = requests.get(endpoint)
+	txt = r.text
+	txt = txt.split("<br />")
+	hosts = []
+	for item in txt:
+		if "<b>" in item:
+			continue
+		if "hosts" in item:
+			res = re.search('/hosts/(.*)">', item)
+			h = res.group(1)
+			hosts.append(h)
+	hstr = ""
+	for item in hosts:
+		hstr += item + "\n"	
+	return hstr
+
+
+def get_host(srv, host):
+	endpoint = "http://" + srv + "/api/hosts/" + host
+	r = requests.get(endpoint)
+	txt = r.text
+	if "No data on host" in txt:
+		return "404"
+	txt = txt.split("<br />")
+	
+	a = "\n"
+	for line in txt:
+		#print(line)
+		line = line.replace("<p>", '')
+		line = line.replace("</p>", '')
+		a += line
+	return a
+
+def help_msg():
+	s = """	'help': print help message
+	'hosts':list tracked hosts
+	'show X'show IP tables for hostname X
+	'exit': close the program"""	
+	return s	
+
+def inp_loop(srv):
+	inp = input("Tracker> ")
+	if inp == "help":
+		st = help_msg()
+	elif inp == "hosts":
+		st = list_hosts(srv)
+	elif "show" in inp:
+		h = inp.lstrip("show ")
+		r = get_host(srv, h)
+		if r == "404":
+			st = "No data for host: " + h
+		else:
+			st = r
+	elif inp == "":
+		return
+	elif inp == "exit":
+		exit()
+	else:
+		st = "Invalid input, type 'help' for assistance"
+	
+	print(st)	
+		
+
+while True:
+	inp_loop(server)
+
+
+
+
