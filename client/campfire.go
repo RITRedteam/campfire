d2e6dc52e0cf710c5f2c44ab6dd41e3924fe6f08
degenerat3 <james.maskelony@gmail.com>
2019-02-15 21:06:07 -0500

    var names refactor

--- client/campfire.go
+++ client/campfire.go
@@ -4,60 +4,59 @@
 // @author: degenerat3
 package main
 
-import "os/exec"
-import "fmt"
-import "log"
-import "net/http"
-// import "io/ioutil"
-import "bytes"
-import "encoding/json"
-import "strings"
-import "time"
-import "os"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
 
-var serv = "127.0.0.1:5000"	//IP of flask serv
-var loop_time = 60		//sleep time in seconds
-
+var serv = "127.0.0.1:5000" //IP of flask serv
+var loopTime = 60           //sleep time in seconds
 
 // return output of "iptables -L" as one large string
-func get_tables() string{
+func getTables() string {
 	cmd := exec.Command("/bin/bash", "-c", "echo \"Filter Table\"; iptables -t filter -L; echo; echo; echo \"NAT Table\"; iptables -t nat -L; echo; echo; echo \"Mangle Table\"; iptables -t mangle -L; echo; echo; echo \"Raw Table\"; iptables -t raw -L;")
-    out, err := cmd.CombinedOutput()
-    if err != nil {
-        log.Fatalf("cmd.Run() failed with %s\n", err)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
 		return "Err"
-    }
+	}
 	return string(out)
 }
 
-
 // return hostname as string
-func get_hn() string{
-    return os.Hostname()
+func getHn() string {
+	hstr, _ := os.Hostname()
+	return hstr
 
 }
 
-
-func get_ip() string{
+func getIP() string {
 	conn, _ := net.Dial("udp", "8.8.8.8:80")
 	defer conn.Close()
 	ad := conn.LocalAddr().(*net.UDPAddr)
-	ip_str := ad.IP.String()
-	ip_str = strings.Replace(ip_str, ".", "-")
-	return ip_str
+	ipStr := ad.IP.String()
+	ipStr = strings.Replace(ipStr, ".", "-", -1)
+	return ipStr
 }
 
-
 // post strings to flask server
-func send_data(rules string, host string){
-	url1 := "http://" + serv + "/api/rule_send"	// turn ip into valid url
+func sendData(rules string, host string, ip string) {
+	url1 := "http://" + serv + "/api/rule_send" // turn ip into valid url
 	jsonData := map[string]string{"hostname": host, "rules": rules, "ip": ip}
 	jsonValue, _ := json.Marshal(jsonData)
 	_, err := http.Post(url1, "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil {
 		fmt.Printf("Req failed: %s\n", err)
 		return
-	} else{
+	} else {
 		// block below for debug
 		// data, _ := ioutil.ReadAll(resp.Body)
 		// fmt.Println(string(data))
@@ -65,26 +64,23 @@ func send_data(rules string, host string){
 	}
 }
 
-
 // fetch data then send it
-func run(){
-	rules := get_tables()
-	host := get_hn()
-	ip := get_ip()
-	send_data(rules, host, ip)
+func run() {
+	rules := getTables()
+	host := getHn()
+	ip := getIP()
+	sendData(rules, host, ip)
 }
 
-func main(){
-	loop_arg := os.Args[1]
-	if loop_arg == "-s"{	// if "-s" is an arg, run once, otherwise loop
+func main() {
+	loopArg := os.Args[1]
+	if loopArg == "-s" { // if "-s" is an arg, run once, otherwise loop
 		run()
-	} else{
-		for {				// send data to webserver ever X seconds, until termination
+	} else {
+		for { // send data to webserver ever X seconds, until termination
 			run()
-			t := time.Duration(loop_time*1000)
+			t := time.Duration(loopTime * 1000)
 			time.Sleep(t * time.Millisecond)
 		}
 	}
 }
-
-
